fe855dab47268f97f96f2caba99bfc63d589b685
ALLENWAN1 <wangyuxiang9132@gmail.com>
2026-01-26 21:54:23 +0100

    roll back

--- agents/assistant_agent/agent.py
+++ agents/assistant_agent/agent.py
@@ -60,7 +60,7 @@ root_run_config = RunConfig(
 
 root_agent = Agent(
     name="assistant_agent",
-    model="gemini-2.5-flash",
+    model="gemini-live-2.5-flash-native-audio",
     description="Livia, assistante IA d'Emmanuel Prat",
     instruction=instruction,
     tools=[send_voicemail_email, update_voicemail_data]
